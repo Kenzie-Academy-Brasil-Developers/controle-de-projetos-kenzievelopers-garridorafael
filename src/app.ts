d2e892c0fc7d5b455fad3d5be1ab51af77f979ab
Rafael <rgarrido846@gmail.com>
2023-08-10 18:34:49 -0300

    feat: define routes of project

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
-import express, { Application } from "express";
+import "express-async-errors"
+import express, { Application, json } from "express";
 import "dotenv/config";
+import { developerRouter } from "./Routers";
+import { handleErrors } from "./Middlewares/handleErrors.middleware";
+import projectRouter from "./Routers/projects.router";
 
 const app: Application = express();
+app.use(json());
+
+app.use("/developers", developerRouter);
+app.use("/projects", projectRouter);
+app.use(handleErrors);
 
 export default app;
